86905e68823774febb4d824ec59e7f73e13ff831
Алексей Арх <a@arh.one>
2019-02-14 12:12:22 +0300

    config

---
+++ configuration/construction.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'Caser' => [
+        'class' => 'arhone\conversion\caser\Caser'
+    ],
+    'Stemmer' => [
+        'class' => 'arhone\conversion\stemmer\Stemmer'
+    ]
+];
